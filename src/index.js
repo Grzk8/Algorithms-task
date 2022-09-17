75e61f083062e46825a5f4cdc66ab576e74fe96e
Grzegorz Kaczor <grzesiek125@op.pl>
2022-09-17 09:43:30 +0200

    iot with users

--- src/index.js
+++ src/index.js
@@ -8,9 +8,35 @@ const path = require("path");
 
   console.log(new Date().toISOString());
   console.log(count(users, mobileDevices, iotDevices));
-  console.log(new Date().toISOString());
+  //console.log(new Date().toISOString());
 })();
 
 function count(users, mobileDevices, iotDevices) {
-  // your code goes here
+
+  let iotWithUsers = [];
+  const user = users.map(u => {
+    return { name: u.name.split(' ', 1), nameId: u.id };
+  });
+  user.sort();
+
+  const mobile = mobileDevices.map(m => {
+    return { mobileId: m.id, userId: m.user }
+  });
+
+  const iot = iotDevices.map(i => {
+    return { iotId: i.id, mobileId: i.mobile }
+  });
+
+  for (let i = 0; i < iot.length; i++) {
+    console.log(iot[i].mobileId);
+    for (let j = 0; j < mobile.length; j++) {
+      for (let k = 0; k< user.length; k ++) {
+        if (iot[i].mobileId === mobile[j].mobileId && mobile[j].userId === user[k].nameId) {
+          iotWithUsers.push({iotId: iot[i].iotId, iotMobileId: iot[i].mobileId, mobileId: mobile[j].mobileId, mobileUserId: mobile[j].userId, userId: user[k].nameId, name: user[k].name})
+        };
+      };
+    };
+  };
+
+  return iotWithUsers;
 }
